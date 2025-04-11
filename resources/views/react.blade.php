d635bf026467d3f8a9f54ce3393232f51148c9b3
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2025-04-12 02:18:51 +0530

    change manifest2

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -12,7 +12,7 @@
 <body>
     <div id="root"></div>
     <!-- Link to the JS file -->
-    <script src="{{ secure_asset('react/assets/index-DhdGdqdt.js') }}"></script>
+    <script src="{{ secure_asset('react/assets/index-DKbhaLpF.js') }}"></script>
 </body>
 
 </html>
