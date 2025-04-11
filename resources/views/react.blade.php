ee252bbb295d54171a3650f330a0c92b5cc5b43c
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2025-04-12 01:36:54 +0530

    change manifest

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel + React</title>
     <!-- Link to the CSS file -->
-    <link href="{{ asset('react/assets/index-nr7EtYML.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ secure_asset('react/assets/index-nr7EtYML.css') }}">
 </head>
 
 <body>
     <div id="root"></div>
     <!-- Link to the JS file -->
-    <script src="{{ asset('react/assets/index-DhdGdqdt.js') }}"></script>
+    <script src="{{ secure_asset('react/assets/index-DhdGdqdt.js') }}"></script>
 </body>
 
 </html>
