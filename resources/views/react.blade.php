140a419d897c3b7add2d857d64eb6b4337a9d1ab
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2025-04-12 01:31:31 +0530

    change made in fe

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -12,7 +12,7 @@
 <body>
     <div id="root"></div>
     <!-- Link to the JS file -->
-    <script src="{{ asset('react/assets/index-V9IlMDp4.js') }}"></script>
+    <script src="{{ asset('react/assets/index-DhdGdqdt.js') }}"></script>
 </body>
 
 </html>
