77b03e316b66f67247328b74d29cad7ca40d4adc
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2025-04-21 09:53:13 +0530

    Latest

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel + React</title>
     <!-- Link to the CSS file -->
-    <link rel="stylesheet" href="{{ secure_asset('react/assets/index-nr7EtYML.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('react/assets/index-Ba7Lu6RB.css') }}">
 </head>
 
 <body>
     <div id="root"></div>
     <!-- Link to the JS file -->
-    <script src="{{ secure_asset('react/assets/index-5xPRQBdM.js') }}"></script>
+    <script src="{{ secure_asset('react/assets/index-DrXCEf86.js') }}"></script>
 </body>
 
 </html>
