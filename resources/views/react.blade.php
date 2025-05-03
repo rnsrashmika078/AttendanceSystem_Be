5180687cc2e86148b6b610b06e5d18a820e9b855
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2025-05-03 20:55:48 +0530

    minor Changes to Dashsboard V.1.1.1

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -12,7 +12,7 @@
 <body>
     <div id="root"></div>
     <!-- Link to the JS file -->
-    <script src="{{ secure_asset('react/assets/index-DfDDHxN2.js') }}"></script>
+    <script src="{{ secure_asset('react/assets/index-DYPvXMfQ.js') }}"></script>
 </body>
 
 </html>
